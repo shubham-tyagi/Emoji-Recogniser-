3e1beefc968073b0bb3b0925e0054dfb36ed37f1
shubham-tyagi <raj.tyagi2000@gmail.com>
2021-09-27 19:10:53 +0530

    initial commit, feat :whole app completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-	return <h1> Hello </h1>;
+	const emojiDictionary = {
+		"😊": "Smiling",
+		"😳": "disbelief",
+		"😔": "sad",
+		"🥡": "takeout box",
+		"❤️": "love",
+		"😑": "annoyance" /** add some more to show how the app now expands when there's new data */,
+	};
+	// console.log(emojiDictionary["😊"]);
+	const [meaning, setMeaning] = useState("");
+
+	const onClickHandler = function (item) {
+		setMeaning(emojiDictionary[item]);
+	};
+
+	const onInput = function (event) {
+		let item = event.target.value;
+		setMeaning(emojiDictionary[item]);
+	};
+
+	const emojis = Object.keys(emojiDictionary);
+
+	return (
+		<div className="app">
+			<h1 style={{ textAlign: "center" }}> Emoji Recogniser </h1>
+			<input className="usr-inp" onChange={onInput} />
+			<h3 class="emojis-meaning">{meaning}</h3>
+			<h3 className="emojis-description-header">Emojis We Know</h3>
+			<ul className="emoji-list">
+				{emojis.map((items) => {
+					return (
+						<li
+							onClick={() => onClickHandler(items)}
+							key={items}
+							className="emoji-list-item"
+						>
+							{items}
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
